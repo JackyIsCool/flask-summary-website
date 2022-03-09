160e80217c4d18e8235521c4ac311a1e52cf7fd5
JackyChanzijian <52320621+JackyChanzijian@users.noreply.github.com>
2022-03-09 13:12:24 +0800

    html post (idk what is this)

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ Happy womens day to all the females in this comment section!
 Also once again loved your video mysticat, keep growing your channel with your amazing content, I have a suggestion, can you make these cursed things into a datapack, like just run one command and see the magic happening and armor stands spawning!, you could even customise it for all shapes, like you accidentally did for the hexagon!
 I would really love to try the circle and this triangle, and even the shark, but Im too dumb too successfully copy you lol
 """
-
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        input = request.form['name']
+        return render_template("index.html", summary=getSummary(input))
     return render_template("index.html", summary=getSummary(article))
 
 @app.route("/<article>")
